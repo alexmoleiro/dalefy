7850332ace9dddc4bca39a8aa33a5908107be980
Alejandro Moleiro <amoleiro@thoughtworks.com>
2018-04-25 11:23:42 +0200

    Creando ProductLine

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-import imagen from './assets/bambas.png';
 import './App.css';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
+import ProductLine from 'ProductLine';
 
 const store = createStore(
     (state, action) => state,
@@ -15,31 +15,12 @@ class App extends Component {
         alert("Hola payo");
     }
 
-    pintar_linea() {
-        return (
-            <div className="row">
-                <div className="col-3 float-left">
-                    <img style={{width: '100%'}} src={imagen}/>
-                </div>
-                <div className="col-8 float-left">
-                    <div>No hay mucho texto
-                    </div>
-                    <button type="button" onClick={this.openAlert.bind()} className="btn btn-success">Click to test how
-                        it
-                        works
-                    </button>
-                </div>
-            </div>);
-    }
-
     render() {
         return (
             <Provider store={store}>
-
                 <div className="App">
                     <div className="container">
-                        {this.pintar_linea()}
-                        {this.pintar_linea()}
+                        <ProductLine open={this.openAlert}/>
                     </div>
                 </div>
             </Provider>
--- src/ProductLine.js
+++ src/ProductLine.js
@@ -1,3 +1,21 @@
-/**
- * Created by alex on 25/04/2018.
- */
+import imagen from './assets/bambas.png';
+
+const ProductLine = (props) => {
+
+   return (
+        <div className="row">
+            <div className="col-3 float-left">
+                <img style={{width: '100%'}} src={imagen}/>
+            </div>
+            <div className="col-8 float-left">
+                <div>No hay mucho texto
+                </div>
+                <button type="button" onClick={this.props.open.bind()} className="btn btn-success">Click to test how
+                    it
+                    works
+                </button>
+            </div>
+        </div>)
+}
+
+export default ProductLine;
