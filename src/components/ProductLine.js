ec5b04f818f4650fa2c1a609172224335fe71235
Alejandro Moleiro <amoleiro@thoughtworks.com>
2018-05-09 11:20:46 +0200

    Nice textfields

--- src/components/ProductLine.js
+++ src/components/ProductLine.js
@@ -2,20 +2,20 @@ import React, {Component} from 'react';
 import {withStyles} from 'material-ui/styles';
 import Typography from 'material-ui/Typography';
 import Button from 'material-ui/Button';
-import {ListItem} from 'material-ui/List';
 import Divider from 'material-ui/Divider';
 import Grid from 'material-ui/Grid';
 import TextField from 'material-ui/TextField';
 import {connect} from 'react-redux';
 import {updateFile} from './../actions/googleDriveActions';
+import List, {ListItem, ListItemIcon, ListItemText} from 'material-ui/List';
 
-const styles = theme => ({
+/*const styles = theme => ({
     root: {
         width: '100%',
         maxWidth: 360,
         backgroundColor: theme.palette.background.paper,
     },
-});
+});*/
 class ProductLine extends Component {
 
     constructor(props) {
@@ -31,12 +31,11 @@ class ProductLine extends Component {
     handleChange(event, field) {
         const {name, description} = this.state.form;
 
-        if (field === "name")
-        {
+        if (field === "name") {
             this.setState({form: {name: event.target.value, description}})
         }
 
-        if (field === "description"){
+        if (field === "description") {
             this.setState({form: {description: event.target.value, name}})
         }
     }
@@ -52,19 +51,28 @@ class ProductLine extends Component {
                             <img alt="" src={product.thumbnailLink}/>
                         </Grid>
                         <Grid item xs={12} sm={9} className="productbody">
-                            <TextField value={this.state.form.name}
-                                       onChange={(event) => this.handleChange(event, "name")}/>
-                            <TextField value={this.state.form.description}
-                                       onChange={(event) => this.handleChange(event, "description")}/>
-                            <Button variant="raised" component="span"
-                                    onClick={() => updateFile(product.id, this.state.form)}>
-                                Update
-                            </Button>
+                            <List>
+                                <ListItem >
+                                    <TextField label="Name" value={this.state.form.name}
+                                               onChange={(event) => this.handleChange(event, "name")}/>
+                                </ListItem>
+                                <ListItem>
+                                    <TextField label="Description" multiline rowsMax="4" fullWidth value={this.state.form.description}
+                                               onChange={(event) => this.handleChange(event, "description")}/>
+                                </ListItem>
+                                <ListItem >
+                                    <Button variant="raised" component="span"
+                                            onClick={() => updateFile(product.id, this.state.form)}>
+                                        Update
+                                    </Button>
+                                </ListItem>
+                            </List>
                         </Grid>
                     </Grid>
                 </ListItem>
                 <Divider/>
-            </div>)
+            </div>
+        )
     }
 }
 
@@ -74,4 +82,5 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 
-export default connect(null, mapDispatchToProps)(withStyles(styles)(ProductLine));
+// export default connect(null, mapDispatchToProps)(withStyles(styles)(ProductLine));
+export default connect(null, mapDispatchToProps)(ProductLine);
